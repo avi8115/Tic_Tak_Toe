42feb3b8d67ee0ae700bf846c476b7f80ad4efb1
Avinash Kumar <87919237+avi8115@users.noreply.github.com>
2023-06-16 11:57:35 +0530

    adding java code for this game

---
+++ tik_tak_toe.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class tik_tak_toe {
+
+    public static void printBoard(char board[][]){
+        for(int row=0; row<board.length; row++){
+            for(int col=0; col<board[row].length; col++){
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+        }
+    }
+    public static boolean haveWon(char board[][], char player){
+        //Check for rows
+        for(int row=0; row<board.length; row++){
+            if(board[row][0] == player && board[row][1] == player && board[row][2] == player){
+                return true;
+            }
+        }
+        //check for col
+        for(int col=0; col<board[0].length; col++){
+            if(board[0][col] == player && board[1][col] == player && board[2][col] == player){
+                return true;
+            }
+        }
+        // check Diagonal
+        if(board[0][0] == player && board[1][1] == player && board[2][2] == player){
+            return true;
+        }
+        if(board[0][2] == player && board[1][1] == player && board[2][0] == player){
+            return true;
+        }
+        return false;   
+
+    }
+    public static void main(String args[]){
+        char board[][] = new char[3][3];
+        for(int row=0; row<board.length; row++){
+            for(int col=0; col<board[row].length; col++){
+                board[row][col]=' ';
+            }
+        }
+        char player = 'X';
+        boolean gameOver = false;
+        Scanner sc = new Scanner(System.in);
+
+        while(!gameOver){
+            printBoard(board);
+            System.out.println("Player " + player + " enter: ");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+
+            if(board[row][col]==' '){
+                //place the element
+                board[row][col] = player;
+                gameOver = haveWon(board, player);
+                if(gameOver){
+                    System.out.println("Player " + player + " has won: ");
+                }else{
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+            }else{
+                System.out.println("Invalid Move !!! try again.");
+            }
+        }
+        printBoard(board);
+
+    }
+}
